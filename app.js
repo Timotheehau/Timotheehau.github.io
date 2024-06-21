7b264f2fa3f5db26886c85cd7ddb05a18544b3dd
timotheehau <t.hauser@ecole-ipssi.net>
2024-06-21 17:21:11 +0200

    Portfolio last steps

--- app.js
+++ app.js
@@ -6,21 +6,21 @@ const CV = document.getElementById('CV')
 const LM = document.getElementById('LM')
 
 goUp.id = 'goUp'
-goUp.innerHTML = '&#8593;'
+goUp.innerHTML = '&#8593'
 
-goUp.style.position = 'fixed';
-goUp.style.bottom = '20px'; // Ajustez la position verticale selon vos besoins
-goUp.style.right = '20px'; // Ajustez la position horizontale selon vos besoins
-goUp.style.fontSize = '24px'; // Ajustez la taille de la flèche selon vos besoins
-goUp.style.cursor = 'pointer'; // Changez le curseur pour indiquer qu'il est cliquable
-goUp.style.backgroundColor = 'orange'; // Changez la couleur d'arrière-plan de la flèche
-goUp.style.color = 'white'; // Changez la couleur du texte de la flèche
-goUp.style.padding = '10px'; // Ajoutez un peu d'espace autour du texte de la flèche
+goUp.style.position = 'fixed'
+goUp.style.bottom = '20px' // Ajustez la position verticale selon vos besoins
+goUp.style.right = '20px' // Ajustez la position horizontale selon vos besoins
+goUp.style.fontSize = '24px' // Ajustez la taille de la flèche selon vos besoins
+goUp.style.cursor = 'pointer' // Changez le curseur pour indiquer qu'il est cliquable
+goUp.style.backgroundColor = 'orange' // Changez la couleur d'arrière-plan de la flèche
+goUp.style.color = 'white' // Changez la couleur du texte de la flèche
+goUp.style.padding = '10px' // Ajoutez un peu d'espace autour du texte de la flèche
 goUp.style.borderRadius = '50%'
 
 openBtn.onclick = openNav
 closeBtn.onclick = closeNav
-var footer = document.getElementById('footer');
+var footer = document.getElementById('footer')
 footer.parentNode.insertBefore(goUp, footer.nextSibling)
 
 
@@ -33,11 +33,11 @@ function closeNav() {
 }
 
 CV.addEventListener('click', () => {
-    window.location.href = 'Alternance-Développeur-Web.webp';
+    window.location.href = 'Alternance-Développeur-Web.webp'
 })
 
 LM.addEventListener('click', () => {
-    window.location.href = 'lm-Timothée-Hauser-–-V2.webp';
+    window.location.href = 'lm-Timothée-Hauser-–-V2.webp'
 })
 
 openBtn.addEventListener('click', () => {
